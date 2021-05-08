9616366afe0ed9ad8b0b4f29ac56a5eb532c48d2
Vidhi1109 <80022666+Vidhi1109@users.noreply.github.com>
2021-05-08 13:33:09 +0530

    Debugged Move code

--- Directory.c
+++ Directory.c
@@ -1,4 +1,5 @@
 #include "Directory.h"
+#include "hash.h"
 
 PtrTree MakeTree()
 {
@@ -13,11 +14,11 @@ PtrTree MakeTree()
     return T;
 }
 
-Node *MakeNode(Node *parentAddress, PtrTree Tree, char *inputString, bool inputType)
+Node *MakeNode(Node *parentAddress, PtrTree root, char *inputString, bool inputType)
 {
     Node *N = (Node *)malloc(sizeof(Node));
     N->FirstChild = NULL;
-    N->name = (char *)malloc(sizeof(char) * strlen(inputString));
+    N->name = (char *)malloc(sizeof(char *));
     N->name = inputString;
     N->Parent = parentAddress;
     N->Sibling = parentAddress->FirstChild;
@@ -26,24 +27,49 @@ Node *MakeNode(Node *parentAddress, PtrTree Tree, char *inputString, bool inputT
     return N;
 }
 
-void AddFile(PtrNode current, PtrTree Tree, char *inputString)
+void AddFile(PtrNode current, PtrTree root, char *inputString)
 {
-    Node *File = MakeNode(current, Tree, inputString, 1); //file inputType is 1
+    Node *File = MakeNode(current, root, inputString, 1); //file inputType is 1
 }
-void AddDirectory(PtrNode current, PtrTree Tree, char *inputString)
+void AddDirectory(PtrNode current, PtrTree root, char *inputString)
 {
-    Node *Folder = MakeNode(current, Tree, inputString, 0); //directory inputType is 0
+    Node *Folder = MakeNode(current, root, inputString, 0); //directory inputType is 0
 }
-
-PtrNode search(PtrNode current, char *array)
+/*
+PtrNode searchAlias (PtrNode root , char* Alias)
 {
-    if (current->type == 1)
+    PtrNode current = root;
+    if(current->type==1)
     {
         return NULL;
     }
     PtrNode head = current->FirstChild;
+    PtrNode temp = head;
+    while(head!=NULL)
+    {
+        if(strcmp(head->Alias , Alias)==0)
+        {
+            return head;
+        }
+        head=head->Sibling;
+        return searchAlias(head->FirstChild, Alias);
+    }
+    return searchAlias(temp->FirstChild, Alias);
+}*/
+
+PtrNode search(PtrNode current, char *array)
+{
+    //printf("%s %s\n",current->name , array);
+    if (current->type == 1)
+    {
+        return NULL;
+    }
+    PtrNode head = current->FirstChild; /// root->c1->c2->c3
+                                        //\c4->c5->6
+    //printf("%p",current->FirstChild);
     while (head != NULL)
     {
+        //printf("%s %s\n",head->name , array);
         if (strcmp(head->name, array) == 0)
         {
             if (head->type == 1)
@@ -56,7 +82,7 @@ PtrNode search(PtrNode current, char *array)
         }
         head = head->Sibling;
     }
-    printf("There exists no directory named '%s' in parent directory '%s'\n",array ,current->name);
+    printf("There exists no directory named '%s' in parent directory '%s'\n", array, current->name);
     return NULL;
 }
 
@@ -65,6 +91,10 @@ PtrNode Move(PtrTree Tree, char *inputString)
     PtrNode parent = Tree->root;
     PtrNode current = Tree->root;
     char array[100];
+    if(strcmp(inputString , "root")==0)
+    {
+        return current;
+    }
     for (int i = 0, j = 0; i < strlen(inputString); i++, j++)
     {
         if (inputString[i] == '/')
@@ -98,7 +128,121 @@ PtrNode Move(PtrTree Tree, char *inputString)
     }
     return current;
 }
+void traverseTree(PtrNode root, char *prefix)
+{
+    if (root == NULL)
+        return;
 
+    while (root)
+    {
+        int m=1;
+        for (int i = 0; i < strlen(prefix); i++)
+        {
+            if (root->name[i] == prefix[i])
+            {
+                m++;
+            }
+            else
+            {
+                break;
+            }
+        }
+        if (m == strlen(prefix) + 1)
+        {
+            printf("%s\n",root->name);
+        }
+        if (root->FirstChild)
+            traverseTree(root->FirstChild , prefix);
+        root = root->Sibling;
+    }
+}
+/*
+PtrNode MoveAlias(PtrNode root, char* Alias)
+{
+    PtrNode current = searchAlias(current, Alias);
+    return current;
+}*/
+
+HT2 **MakeHashTable()
+{
+    struct Node2 **HT2 = (struct Node2 **)malloc(sizeof(struct Node2 *) * 2003);
+    for (int i = 0; i < 2003; i++)
+    {
+        HT2[i] = NULL;
+    }
+
+    return HT2;
+}
+
+void StoreAlias(PtrTree Tree, char *Address, char *Alias, HT2 **AliasHashTable)
+{
+    PtrNode N = Move(Tree, Address);
+    if (N == NULL)
+        return;
+    if (N != NULL)
+    {
+        printf("The file/directory at location '%s' has been stored with alias '%s'\n", Address, Alias);
+    }
+    insertSep2(Address, AliasHashTable, Alias);
+}
+/*
+void StoreAliasSafety(PtrTree root,char* Address,char* Alias)
+{
+    PtrNode input = Move(root,Address);
+    strcpy(input->Alias,Alias);
+}*/
+PtrNode Teleport(PtrTree Tree, char *alias, HT2 **AliasHashTable)
+{
+    char *NewAddress;
+    NewAddress = searchSepAlias(alias, AliasHashTable);
+    if (NewAddress == NULL)
+    {
+        printf("The alias '%s' does not exist", alias);
+        return NULL;
+    }
+    else
+    {
+        PtrNode directory = Move(Tree, NewAddress);
+        return directory;
+    }
+}
+
+void find(char STRING[], int n, char Array[][1000])
+{
+
+    int len = n;
+    //hash_for_find[];
+    int j = 0;
+    while (j < 10) //Number of inputs
+    {
+
+        int m = 1;
+
+        for (int i = 0; i < len; i++)
+        {
+            if (Array[j][i] == STRING[i])
+            {
+                m++;
+            }
+            else
+            {
+                break;
+            }
+        }
+        if (m == len + 1)
+        {
+            printf("%s\n", Array[j]);
+        }
+        j++;
+    }
+    //optimiztion can be done with the while loop as for how many times to run it
+}
+/*
+PtrNode TeleportSafety(char* Alias, PtrTree root)
+{
+    PtrNode current = MoveAlias(root, Alias);
+    return current;
+}*/
 void Quit()
 {
     printf("Goodbye\n");
